3baa301fb1db2e2764421d2cffda3303d1b1040b
Sarawoot Khunsri <toom_sarawoot@hotmail.com>
2022-02-15 15:45:18 +0700

    added resume content

---
+++ ResumeApp/Screens/Resume/Content/View/ResumeContentViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ResumeContentViewController.swift
+//  ResumeApp
+//
+//  Created by Sarawoot Khunsri on 15/2/2565 BE.
+//
+
+import UIKit
+
+class ResumeContentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

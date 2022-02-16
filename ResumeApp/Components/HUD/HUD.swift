1fc2f21eb8c96c124657a7139b0b335d095a0f9d
Sarawoot Khunsri <toom_sarawoot@hotmail.com>
2022-02-17 01:17:23 +0700

    update component

--- ResumeApp/Components/HUD/HUD.swift
+++ ResumeApp/Components/HUD/HUD.swift
@@ -16,14 +16,21 @@ struct HUD {
         }
     }
     
-    static func sheet(for viewController: UIViewController?, editAction: ((UIAlertAction) -> Void)? = nil, deleteAction: ((UIAlertAction) -> Void)? = nil, cancelAction: ((UIAlertAction) -> Void)? = nil) {
+    static func sheet(for viewController: UIViewController?, isViewContent: Bool = false, viewContent: ((UIAlertAction) -> Void)? = nil, editAction: ((UIAlertAction) -> Void)? = nil, deleteAction: ((UIAlertAction) -> Void)? = nil, cancelAction: ((UIAlertAction) -> Void)? = nil) {
         let alertSheet = UIAlertController(title: "What do you want to do with item?", message: nil, preferredStyle: .actionSheet)
         let edit = UIAlertAction(title: "Edit", style: .default, handler: editAction)
         let delete = UIAlertAction(title: "Delete", style: .destructive, handler: deleteAction)
+        
         let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: cancelAction)
         alertSheet.addAction(cancel)
+        if isViewContent {
+            let viewContent = UIAlertAction(title: "View Content", style: .default, handler: viewContent)
+            alertSheet.addAction(viewContent)
+        }
         alertSheet.addAction(edit)
         alertSheet.addAction(delete)
         viewController?.present(alertSheet, animated: true, completion: nil)
     }
+    
+    
 }

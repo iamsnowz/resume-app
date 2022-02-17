b0e8c8bc5e7e1b0f6a74e3e0c397f2a6258bf4e2
Sarawoot Khunsri <toom_sarawoot@hotmail.com>
2022-02-17 21:31:01 +0700

    improvements to support unit-testing

--- ResumeApp/Screens/Resume/List/ViewModel/ResumeListViewModel.swift
+++ ResumeApp/Screens/Resume/List/ViewModel/ResumeListViewModel.swift
@@ -56,14 +56,14 @@ final class ResumeListViewModel: ResumeListViewModelInput, ResumeListViewModelOu
     
     func delete(at indexPath: IndexPath) {
         let id = resume(at: indexPath).id
-        service.deleteResume(withId: id)
+        service.deleteResume(withId: id, nil)
     }
     
     func fetchResumeList() {
         service.observe { [weak self] result in
             switch result {
-            case .success(let results):
-                self?.prepareData(results: results)
+            case .success(let model):
+                self?.resumeList = model
             case .failure(let error):
                 switch error {
                 case .notFoundData:
@@ -71,44 +71,6 @@ final class ResumeListViewModel: ResumeListViewModelInput, ResumeListViewModelOu
                 }
             }
         }
-    }
-    
-    private func prepareData(results: Results<ResumeObject>) {
-        var resumeList: [ResumeItem] = []
-        for resumeObject in results {
-            let personalDetail = PersonalDetailItem(profileImageData: resumeObject.profileImageData,
-                                                    resumeTitle: resumeObject.resumeTitle,
-                                                    firstname: resumeObject.firstname,
-                                                    lastname: resumeObject.lastname,
-                                                    mobileNumber: resumeObject.mobileNumber,
-                                                    emailAddress: resumeObject.emailAddress,
-                                                    residenceAddress: resumeObject.residenceAddress,
-                                                    careerObjective: resumeObject.careerObjective,
-                                                    totalYearsOfExperience: resumeObject.totalYearsOfExperience)
-            let workSummary: [WorkSummaryItem] = RealmService.shared.realm.objects(WorkSummaryObject.self).where {
-                $0.resumeId == resumeObject.id
-            }.map { WorkSummaryItem(company: $0.companyName, startDate: $0.startDate, endDate: $0.endDate) }
-            
-            let skills: [String] = RealmService.shared.realm.objects(SkillObject.self).where {
-                $0.resumeId == resumeObject.id
-            }.map { $0.name }
-            
-            let educationDetails: [EducationDetailItem] = RealmService.shared.realm.objects(EducationDetailObject.self).where {
-                $0.resumeId == resumeObject.id
-            }.map { EducationDetailItem(grade: $0.grade, startYear: $0.startYearDate, endYear: $0.endYearDate, cgpa: $0.cgpa) }
-            
-            let projectDetails: [ProjectDetailItem] = RealmService.shared.realm.objects(ProjectDetailObject.self).where {
-                $0.resumeId == resumeObject.id
-            }.map { ProjectDetailItem(projectName: $0.projectName, teamSize: $0.teamSize, projectSummary: $0.projectSummary, technologyUsed: $0.technologyUsed, role: $0.role) }
-            
-            resumeList.append(ResumeItem(id: resumeObject.id,
-                                         personalDetail: personalDetail,
-                                         workSummary: workSummary,
-                                         skills: skills,
-                                         educationDetail: educationDetails,
-                                         projectDetail: projectDetails))
-        }
-        self.resumeList = resumeList
     }
 }
 
--- ResumeApp/Services/ResumeService.swift
+++ ResumeApp/Services/ResumeService.swift
@@ -13,8 +13,8 @@ enum ServiceError: Error {
 }
 protocol ResumeServiceProtocol {
     func createOrUpdate(withResumeItem model: ResumeItem, completion: (() -> Void)?)
-    func deleteResume(withId id: String)
-    func observe(completion: ((Result<Results<ResumeObject>, ServiceError>) -> Void)?)
+    func deleteResume(withId id: String, _ completion: (() -> Void)?)
+    func observe(completion: ((Result<[ResumeItem], ServiceError>) -> Void)?)
 }
 
 final class ResumeService: ResumeServiceProtocol {
@@ -37,7 +37,7 @@ final class ResumeService: ResumeServiceProtocol {
         completion?()
     }
     
-    func deleteResume(withId id: String) {
+    func deleteResume(withId id: String, _ completion: (() -> Void)?) {
         let workManager = WorkSummaryObjectManager()
         let skillManager = SkillObjectManager()
         let educationDetailManager = EducationDetailObjectManager()
@@ -49,17 +49,58 @@ final class ResumeService: ResumeServiceProtocol {
         educationDetailManager.delete(withId: id)
         projectDetailManager.delete(withId: id)
         resumeManager.delete(withId: id)
+        completion?()
     }
     
-    func observe(completion: ((Result<Results<ResumeObject>, ServiceError>) -> Void)?) {
+    func observe(completion: ((Result<[ResumeItem], ServiceError>) -> Void)?) {
         notificationToken?.invalidate()
         let objects = RealmService.shared.realm.objects(ResumeObject.self)
-        notificationToken = objects.observe { change in
+        notificationToken = objects.observe { [weak self] change in
+            guard let strongSelf = self else { return }
             if objects.isEmpty {
                 completion?(.failure(.notFoundData))
             } else {
-                completion?(.success(objects))
+                let result = strongSelf.prepareData(results: objects)
+                completion?(.success(result))
             }
         }
+    }
+    
+    private func prepareData(results: Results<ResumeObject>) -> [ResumeItem] {
+        var resumeList: [ResumeItem] = []
+        for resumeObject in results {
+            let personalDetail = PersonalDetailItem(profileImageData: resumeObject.profileImageData,
+                                                    resumeTitle: resumeObject.resumeTitle,
+                                                    firstname: resumeObject.firstname,
+                                                    lastname: resumeObject.lastname,
+                                                    mobileNumber: resumeObject.mobileNumber,
+                                                    emailAddress: resumeObject.emailAddress,
+                                                    residenceAddress: resumeObject.residenceAddress,
+                                                    careerObjective: resumeObject.careerObjective,
+                                                    totalYearsOfExperience: resumeObject.totalYearsOfExperience)
+            let workSummary: [WorkSummaryItem] = RealmService.shared.realm.objects(WorkSummaryObject.self).where {
+                $0.resumeId == resumeObject.id
+            }.map { WorkSummaryItem(company: $0.companyName, startDate: $0.startDate, endDate: $0.endDate) }
+            
+            let skills: [String] = RealmService.shared.realm.objects(SkillObject.self).where {
+                $0.resumeId == resumeObject.id
+            }.map { $0.name }
+            
+            let educationDetails: [EducationDetailItem] = RealmService.shared.realm.objects(EducationDetailObject.self).where {
+                $0.resumeId == resumeObject.id
+            }.map { EducationDetailItem(grade: $0.grade, startYear: $0.startYearDate, endYear: $0.endYearDate, cgpa: $0.cgpa) }
+            
+            let projectDetails: [ProjectDetailItem] = RealmService.shared.realm.objects(ProjectDetailObject.self).where {
+                $0.resumeId == resumeObject.id
+            }.map { ProjectDetailItem(projectName: $0.projectName, teamSize: $0.teamSize, projectSummary: $0.projectSummary, technologyUsed: $0.technologyUsed, role: $0.role) }
+            
+            resumeList.append(ResumeItem(id: resumeObject.id,
+                                         personalDetail: personalDetail,
+                                         workSummary: workSummary,
+                                         skills: skills,
+                                         educationDetail: educationDetails,
+                                         projectDetail: projectDetails))
+        }
+        return resumeList
     }
 }
